8d10747c77978674650ab4175501192b170cc274
Yared Ayalew <yareda@gmail.com>
2022-07-12 00:41:04 +0400

    Initial commit

---
+++ src/test/java/com/ghadevops/servicetemplate/ServiceTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghadevops.servicetemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
